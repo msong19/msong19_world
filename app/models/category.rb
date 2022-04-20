fa9154bdfc490f43b11b2587aad721d45d463260
Lisa Leung <clleung@andrew.cmu.edu>
2022-04-20 12:55:56 -0400

    saving work

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
 	has_many :photos
-
+	has_many :articles
+	# sorry i forgot to branch earlier
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+  	scope :alphabetical, -> { order('name') }
   
 end
